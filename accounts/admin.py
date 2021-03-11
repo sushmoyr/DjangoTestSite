8c7d69df55aec7a22f597026e6680b71fecb68c9
sushmoy <sushmoyr@gmail.com>
2021-03-12 00:33:35 +0600

    db models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Customer
+from .models import Order
+from .models import Product
+
 # Register your models here.
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
